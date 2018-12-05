6513c255aac006768340701c0a48dabdbcd6b61a
Michael Lazear <lazear@scripps.edu>
2018-12-05 11:09:43 -0800

    Day 05 further speed improvements

--- day05/src/main.rs
+++ day05/src/main.rs
@@ -2,40 +2,33 @@ extern crate util;
 use std::io;
 
 fn part1(data: String) -> Result<usize, ()> {
-    let mut s = data;
-    let mut j = 0;
-    'outer: loop {
-        for i in j..s.len() - 2 {
-            let a = &s[i..i + 1];
-            let b = &s[i + 1..i + 2];
-            if a != b && a.eq_ignore_ascii_case(b) {
-                let mut x = String::from(&s[0..i]);
-                x.push_str(&s[i + 2..]);
-                s = x;
-                j = i - 1;
-                continue 'outer;
+    let mut v = Vec::new();
+    for c in data.chars() {
+        match v.last() {
+            None => v.push(c),
+            Some(l) => {
+                if c != *l && c.eq_ignore_ascii_case(l) {
+                    v.pop();
+                } else {
+                    v.push(c);
+                }
             }
         }
-        break;
     }
-    Ok(s.len())
+    Ok(v.len())
 }
 
 fn part2(data: &str) -> Result<usize, ()> {
-    let mut best = ('a', data.len());
+    let mut best = data.len();
     for i in ('a' as u32)..=('z' as u32) {
         let c = std::char::from_u32(i).ok_or(())?;
         let s = data
             .chars()
             .filter(|a| !a.eq_ignore_ascii_case(&c))
             .collect::<String>();
-        let r = part1(s)?;
-        if r < best.1 {
-            best = (c, r);
-        }
+        best = best.min(part1(s)?);
     }
-    println!("{:?}", best);
-    Ok(best.1)
+    Ok(best)
 }
 
 #[test]
